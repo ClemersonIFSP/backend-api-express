f44f1e45067907c8871c0ac03e0f65142058a324
Clemerson Santos <clemerson.santos@aluno.ifsp.edu.br>
2024-02-09 15:51:39 -0300

    config server express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log('Olá Mundoooooo!')
+import express from 'express';
+import { PORT, HOST } from './config.js';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on  ${HOST}:${PORT}`);
+});

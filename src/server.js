073fcddf5a63890d5a986ac646bfbf47e9368239
Clemerson Santos <clemerson.santos@aluno.ifsp.edu.br>
2024-03-22 15:02:18 -0300

    Add cors middleware and update server.js

--- src/server.js
+++ src/server.js
@@ -1,20 +1,28 @@
-//const express = require('express')
-import express from 'express'
-import {PORT, HOST} from './config.js'
-import userRouter from './routers/userRouter.js'
-import productRouter from './routers/productRouter.js'
-import logger from './middlewares/logger.js'
+import express from "express";
+import { PORT, HOST } from "./config.js";
+import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
+import logger from "./middlewares/logger.js";
+import cors from "cors";
+const app = express();
 
-const app = express()
+app.use(logger);
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:8081",
+      "http://meusite.com",
+    ],
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json());
 
-app.use(logger)
-app.use(express.json())
-
-app.use('/user', userRouter)
-app.use('/product', productRouter)
+app.use("/user", userRouter);
+app.use("/product", productRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${HOST}:${PORT}`)
-})
-
-
+  console.log(`Server running on ${HOST}:${PORT}`);
+});
